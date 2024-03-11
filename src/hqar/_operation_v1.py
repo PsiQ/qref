4060f09afc51ce4b043576ec703b49ebc1ec7de3
Konrad Jałowiecki <kjalowiecki@psiquantum.com>
2024-03-11 17:40:47 +0100

    Fix formatting

--- src/hqar/_operation_v1.py
+++ src/hqar/_operation_v1.py
@@ -100,7 +100,6 @@ class _GenerateV1JsonSchema(GenerateJsonSchema):
         return name.removeprefix("_").replace("V1", "")
 
 
-
 def generate_operation_schema_v1() -> int:
     """Generate Operation schema V1.
 
