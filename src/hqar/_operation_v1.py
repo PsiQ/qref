8f2be6c917fb93e069dabde2bbc4894b61f14050
Konrad Jałowiecki <kjalowiecki@psiquantum.com>
2024-03-11 15:53:23 +0100

    Add initial version of HQAR models and schema generation functions

---
+++ src/hqar/_operation_v1.py
@@ -0,0 +1,108 @@
+"""
+..  Copyright © 2023-2024 PsiQuantum Corp.  All rights reserved.
+    PSIQUANTUM CORP. CONFIDENTIAL
+    This file includes unpublished proprietary source code of PsiQuantum Corp.
+    The copyright notice above does not evidence any actual or intended publication
+    of such source code. Disclosure of this source code or any related proprietary
+    information is strictly prohibited without the express written permission of
+    PsiQuantum Corp.
+
+Pydantic models used for defining V1 schema of Operation.
+"""
+from __future__ import annotations
+
+from typing import Annotated, Any, Literal, Optional, Union
+
+from pydantic import BaseModel, ConfigDict, Field, StringConstraints
+from pydantic.json_schema import GenerateJsonSchema
+
+
+NAME_PATTERN = "[A-Za-z_][A-Za-z0-9_]*"
+NAMESPACED_NAME_PATTERN = rf"{NAME_PATTERN}\.{NAME_PATTERN}"
+
+
+Name = Annotated[str, StringConstraints(pattern=rf"^{NAME_PATTERN}$")]
+NamespacedName = Annotated[str, StringConstraints(pattern=rf"^{NAMESPACED_NAME_PATTERN}")]
+OptionallyNamespacedName = Annotated[
+    str, StringConstraints(pattern=rf"^(({NAME_PATTERN})|({NAMESPACED_NAME_PATTERN}))$")
+]
+_Value = Union[int, float, str]
+
+
+class _PortV1(BaseModel):
+    name: Name
+    direction: Literal["input", "output", "through"]
+    size: Optional[_Value]
+    model_config = ConfigDict(title="Port")
+
+
+class _ConnectionV1(BaseModel):
+    source: OptionallyNamespacedName
+    target: OptionallyNamespacedName
+
+    model_config = ConfigDict(title="Connection", use_enum_values=True)
+
+
+class _ResourceV1(BaseModel):
+    name: Name
+    type: Literal["additive", "multiplicative", "qubits", "other"]
+    value: Union[int, float, str, None]
+
+    model_config = ConfigDict(title="Resource")
+
+
+class _ParamLinkV1(BaseModel):
+    source: Name
+    targets: list[NamespacedName]
+
+    model_config = ConfigDict(title="ParamLink")
+
+
+class OperationV1(BaseModel):
+    """Description of Operation in V1 schema.
+
+    Note:
+        This is NOT a top-level object in the schema. Instead, OperationV1 is wrapped in
+        DocumentRootV1.
+    """
+    name: Name
+    children: list[OperationV1] = Field(default_factory=list)
+    type: Optional[str] = None
+    ports: list[_PortV1] = Field(default_factory=list)
+    resources: list[_ResourceV1] = Field(default_factory=list)
+    connections: list[_ConnectionV1] = Field(default_factory=list)
+    input_params: list[Name] = Field(default_factory=list)
+    local_variables: list[str] = Field(default_factory=list)
+    linked_params: list[_ParamLinkV1] = Field(default_factory=list)
+    meta: dict[str, Any] = Field(default_factory=dict)
+
+    model_config = ConfigDict(title="Operation")
+
+    def __init__(self, **data: Any):
+        super().__init__(**{k: v for k, v in data.items() if v != [] and v != {}})
+
+
+class DocumentRootV1(BaseModel):
+    """Root object in Operation schema V1."""
+    version: Literal["v1"]
+    operation: OperationV1
+
+
+class _GenerateV1JsonSchema(GenerateJsonSchema):
+    def generate(self, schema, mode="validation"):
+        json_schema = super().generate(schema, mode=mode)
+        json_schema["title"] = "FTQC-ready quantum operation"
+        json_schema["$schema"] = self.schema_dialect
+        return json_schema
+
+    def normalize_name(self, name):
+        return name.removeprefix("_").replace("V1", "")
+
+
+def generate_operation_schema_v1():
+    """Generate Operation schema V1.
+
+    The schema is generated from DocumentRootV1 model, and then enriched with
+    additional fields "title" and "$schema".
+    """
+    return DocumentRootV1.model_json_schema(schema_generator=_GenerateV1JsonSchema)
---
+++ tests/hqar/test_schema_validation.py
@@ -0,0 +1,58 @@
+"""
+..  Copyright © 2023-2024 PsiQuantum Corp.  All rights reserved.
+    PSIQUANTUM CORP. CONFIDENTIAL
+    This file includes unpublished proprietary source code of PsiQuantum Corp.
+    The copyright notice above does not evidence any actual or intended publication
+    of such source code. Disclosure of this source code or any related proprietary
+    information is strictly prohibited without the express written permission of
+    PsiQuantum Corp.
+
+Test cases checking that schema matches data that we expect it to match.
+"""
+from pathlib import Path
+
+import pytest
+import yaml  # type: ignore[import-untyped]
+from jsonschema import ValidationError, validate
+
+from hqar import generate_operation_schema
+
+
+def validate_with_v1(data):
+    validate(data, generate_operation_schema(version="v1"))
+
+
+def load_invalid_examples():
+    with open(Path(__file__).parent / "data/invalid_operation_examples.yaml") as f:
+        data = yaml.safe_load(f)
+
+    return [
+        pytest.param(
+            example["input"],
+            example["error_path"],
+            example["error_message"],
+            id=example["description"],
+        )
+        for example in data
+    ]
+
+
+def load_valid_examples():
+    with open(Path(__file__).parent / "data/valid_operation_examples.yaml") as f:
+        data = yaml.safe_load(f)
+
+    return [pytest.param(example["input"], id=example["description"]) for example in data]
+
+
+@pytest.mark.parametrize("input, error_path, error_message", load_invalid_examples())
+def test_invalid_schema_fail_to_validate(input, error_path, error_message):
+    with pytest.raises(ValidationError) as err_info:
+        validate_with_v1(input)
+
+    assert err_info.value.json_path == error_path
+    assert err_info.value.message == error_message
+
+
+@pytest.mark.parametrize("input", load_valid_examples())
+def test_valid_schema_fail_to_validate(input):
+    validate_with_v1(input)
